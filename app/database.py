ad90bca896596df3b25f0a8b2d121dd57b649c72
Yandy <riandyhsn@gmail.com>
2022-08-03 23:02:10 +0700

    configure database

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://mtdciouacmckni:bc5a36a38fa66dadad7fa22313bca236754cbd8d065580e1153127cb7d0271db@ec2-54-208-104-27.compute-1.amazonaws.com/d1cjc3jptg3kd1"
+DATABASE_URL = 'postgres://mtdciouacmckni:bc5a36a38fa66dadad7fa22313bca236754cbd8d065580e1153127cb7d0271db@ec2-54-208-104-27.compute-1.amazonaws.com:5432/d1cjc3jptg3kd1'
 
 engine = _sql.create_engine(DATABASE_URL)
 
