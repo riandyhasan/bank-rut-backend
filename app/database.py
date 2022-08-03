abdf2cb40ec6aa1860f1fc35c5f052acb6465e86
Yandy <riandyhsn@gmail.com>
2022-08-03 23:46:39 +0700

    change database deployment

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://mtdciouacmckni:bc5a36a38fa66dadad7fa22313bca236754cbd8d065580e1153127cb7d0271db@ec2-54-208-104-27.compute-1.amazonaws.com/d1cjc3jptg3kd1"
+DATABASE_URL = "postgresql://bdrmlwopzvjmbg:3776ab40ba8773df86013ecbf416c7eeceaa1a9a666b1cffb3a06b66fc6782aa@ec2-44-207-126-176.compute-1.amazonaws.com/ddme0h1esrkgpq"
 
 engine = _sql.create_engine(DATABASE_URL)
 
